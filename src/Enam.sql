f446c74aaf462c8a7c474cd8dfb7fb14315dbffe
JandarJavaSenior <140826697+JandarJavaSenior@users.noreply.github.com>
2024-01-22 16:25:33 +0600

    11

---
+++ src/Enam.sql
@@ -0,0 +1,4 @@
+create type Gender as enum('Male','Female');
+create type House_type as enum('House','Apartment');
+create type Genre as enum('DETECTIVE','DRAMA','HISTORY','ROMANS','BIOGRAPHY','FANTASY');
+create type marital_status as enum('Single','Married','Divorced','Widow','Separated','Other');
